b26f1eeb0d05fc38f6eaaa83080ef916581cfcb8
Dev Suthar <4ev3rdk@gmail.com>
2026-03-13 17:23:37 +0530

    Added Clear Button

--- javascript.js
+++ javascript.js
@@ -1,11 +1,15 @@
 let num1 = "";
 let num2 = "";
 let operator = "";
+let result = "";
+let dcml = ".";
 
 const screen = document.querySelector("#screen");
 const nums = document.querySelectorAll(".num");
 const operations = document.querySelectorAll(".operator");
 const equal = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+const backspace = document.querySelector(".backspace");
 
 for (let n of nums) {
     n.addEventListener("click", () => {
@@ -15,6 +19,8 @@ for (let n of nums) {
 
 for (let op of operations) {
     op.addEventListener("click", () => getOperator(op.textContent))
+
+
 }
 
 function getNum(num) {
@@ -30,7 +36,17 @@ function getNum(num) {
 }
 
 function getOperator(op) {
-    operator = op;
+
+    //reminder to learn this again and again I LIKED THIS
+    if (num1 != "" && num2 != "") {
+        operation(operator, num1, num2)
+        num1 = result;
+        num2 = "";
+        screen.value = num1;
+    }
+    else {
+        operator = op;
+    }
 }
 
 function operation(operator, num1, num2) {
@@ -38,20 +54,43 @@ function operation(operator, num1, num2) {
     let a = Number(num1);
     let b = Number(num2);
     if (operator === "+") {
-        return a + b;
+        result = a + b;
+        return result;
+
+
     }
     else if (operator === "-") {
-        return a - b;
+        result = a - b;
+        return result;
+
     }
     else if (operator === "x") {
-        return a * b;
+        result = a * b;
+        return result;
     }
     else if (operator === "/") {
-        return a / b;
+        result = a / b;
+        return result;
     }
 }
 
+
+//result
 equal.addEventListener("click", () => {
-    const result = operation(operator, num1, num2);
+    const results = operation(operator, num1, num2);
+    screen.value = results;
+    num1 = result;
+    num2 = "";
+
+});
+
+//clear button
+clear.addEventListener("click", () => {
+    num1 = "";
+    num2 = "";
+    operator = "";
+    result = "";
     screen.value = result;
 });
+
+//decimal button
